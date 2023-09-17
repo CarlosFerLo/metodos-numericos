e9c51ebf2afe985980e5ececf15f4782140674a9
Carlos Fernandez <carlos.ferlo@outlook.com>
2023-09-17 20:17:23 +0200

    add first intents to resolve the second excercice

---
+++ FernandezCarlos-P1/it2f.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#define K_MAX 34
+
+int main (void) {
+    int i ;
+    float x0, x1, aux ;
+
+    /* Inicializar valores de x0 y x1 */
+    x0 = 11 / 2 ;
+    x1 = 61 / 11 ;
+
+    printf("%3.d %f12.5\n", 0, x0);
+    printf("%3.d %f12.5\n", 1, x1);
+    
+    /* Iterar hasta K_MAX */
+    for (i = 2; i <= K_MAX; i++) {
+        aux = 111 - (1130 - 3000 / x0) / x1 ;
+        x0 = x1 ;
+        x1 = aux ;
+        printf("%3.d %f12.5\n", i, x1) ;
+    }
+
+    return 0 ;
+}
