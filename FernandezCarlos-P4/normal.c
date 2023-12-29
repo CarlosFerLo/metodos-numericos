048a0efcb16be12c78cff48ee61169f6eab32146
Carlos Fernandez <carlos.ferlo@outlook.com>
2023-12-29 16:00:43 +0100

    solve ex 1

--- FernandezCarlos-P4/normal.c
+++ FernandezCarlos-P4/normal.c
@@ -1,32 +1,43 @@
 #include <math.h>
 
 #define TOL 1e-8
+#define F0 0.5
+
+#define POS 1
+#define NEG 0
 
 double normal (double x) {
-	int sign, m, i ;
+	int j, m, signo ;
 	double R, T ;
 
-	/* Calcular solo el caso x > 0 */
+	/* Tomar x como positica y guardar su signo en la variable signo */
 	if (x < 0) {
-		sign = -1 ;
-		x *= -1 ;	
-	} else sign = 1 ;
-
-	/* Calcular valor de h */
+		x *= -1 ;
+		signo = NEG ;
+	} else {
+		signo = POS ;
+	}
+	
+	/* Calcular la integral de 0 a x */
+	/* 	Calcular el valor de m que hace que R sea menor a TOL */
 	m = 1 ;
-	R = (x*x*x * exp(3/2)) / 6 ;
+	R = (x*x*x) / (6 * m*m * exp(3/2)) ;
 	
 	while (R > TOL) {
 		m += 1 ;
-		R *= (m - 1) / m ;
+		R *= ((m+1)*(m+1)) / (m*m) ;
 	}
 
-	/* Calcular T(h) */
-	T = 0.25 + 0.5 * exp(- x*x / 2) ;
-	
-	for (i = 1; i < m; i++) T += exp(- (i*x/m)*(i*x/m) / 2) ;
+	/* Calcular la integral mediante el metodo de trapecios con ese valor d m */
+	T = 0.5 + 0.5 * exp(-(x*x)/2) ;
 
-	/* Devolver f(x) o 1 - f(-x) en funcion del signo de x */
-	if (sign == -1) return 1 - T ;
-	else return T ;
+	for (j = 1; j < m; j++) T += exp(-(j*x/m) * (j*x/m) / 2) ;
+
+	T *= x/m ;
+
+	/* Calcular el valor de f(x) apartir de T */
+	T /= sqrt(2 * M_PI) ;
+	T += F0 ;
+
+	return (signo == POS) ? T : 1 - T ;
 }
---
+++ FernandezCarlos-P4/taula.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#include "normal.h"
+
+#define FILE_NAME_MAX_LEN 20
+
+int main (void) {
+	int j ;
+	char name[FILE_NAME_MAX_LEN] ;
+	FILE *f ;
+	
+	/* Leer el nombre del archivo de escritura */
+	printf("Introduce el nombre del archivo de escritura: ") ;
+	scanf("%s", name) ;
+
+	/* Abrir el archivo de escritura */
+	f = fopen(name, "w") ;
+	if (f == NULL) {
+		printf("ERROR al abrir el archivo '%s'.\n", name) ;
+		return 1 ;
+	}
+
+	/* Imprimir tabla de valores en el archivo */
+	for (j = -40; j <= 40; j++) {
+		fprintf(f, "%le %le\n", j/10., normal(j/10.)) ;
+	}
+
+	/* Cerrar archivo de escritura */
+	fclose(f) ;
+
+	return 0 ;
+}
